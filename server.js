e223a0a5ea4e555b7d3c7a1bd4b5c64f3e4f9c46
Silas Oyewale <silasoyewale10@gmail.com>
2019-12-06 16:44:37 -0800

    portfolio deployment

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express = require ('express');
 const app = express(); //creates a server object. 
 
 //to make the file run, i meed to tell it to listen. listen takes in port number
-
-app.listen(3000, function() {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
     console.log("you started a server on port 3000")
 });    //always going to be 3000. no known application uses this port so developers always use this port for testing. 
 
@@ -15,7 +15,7 @@ app.get('/gingerkisses', function (request, response){
     console.log("someone wants ginger kisses")
     response.send({
         kiss: 'here',
-        from: ginger
+        from: 'ginger'
     })
 })
 //when gingerkisses is visited, the functin triggers
